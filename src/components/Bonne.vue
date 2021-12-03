ccd1ed5ca295f131cf5bbaf1eadde2e3c0b0b2e0
killiankopp <killiankopp@gmail.com>
2021-12-03 09:19:06 +0100

    fisrt commit

---
+++ src/components/Bonne.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'Bonne',
+    props: {
+    msg: String,
+    data: String
+  }
+}
+</script>
+
+<template>
+    <div class="bonne">{{ msg }}</div>
+    <p>{{ data }}</p>
+</template>
+
+<style>
+div.bonne{
+    border: 1px solid red;
+}
+</style>
---
+++ src/views/Toto.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="https://storage.googleapis.com/pebble-v-0/img/logos/pebble-color-256.png">
+  </div>
+  <Bonne msg="Allez..." data="none" />
+</template>
+
+<script>
+// @ is an alias to /src
+import Bonne from '@/components/Bonne.vue'
+
+export default {
+  name: 'Toto',
+  components: {
+    Bonne
+  }
+}
+</script>
