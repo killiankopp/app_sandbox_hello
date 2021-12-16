0205fe27fd4df7d57d7707dde206565773c89c0d
killiankopp <killiankopp@gmail.com>
2021-12-16 21:05:13 +0100

    ajout de axios

--- src/components/Bonne.vue
+++ src/components/Bonne.vue
@@ -1,11 +1,52 @@
 <script>
+
+import axios from 'axios';
+
 export default {
     name: 'Bonne',
     props: {
-    msg: String,
-    data: String
-  }
-}
+        msg: String,
+        data: String
+    },
+    mounted() {
+        console.log('yeeees');
+        let data = new FormData();
+        data.set('login', 'killian');
+        data.set('password', 'quantum2');
+
+        let xhr = axios({
+            url: "https://local.fe.tld/connexion.php",
+            method: 'post',
+            data
+        });
+        
+        xhr.then(response => {
+            this.datas = response.data;
+            document.cookie = `jwt=${response.data.jwt}`;
+            console.log(document.cookie);
+        });
+
+        xhr.catch(function (error) {
+            console.log(error);
+        });
+
+        /*
+        axios.get("https://local.fe.tld/api/", {
+                headers: {
+                'Authorization': 'Bearer' + 'le token',
+                "Content-Type": "application/json",
+                }
+            })
+            .then(response => {
+                this.datas = response.data
+                console.log(response.data);
+            })
+        .catch(function (error) {
+                console.log(error);
+            });
+*/
+        }
+    }
 </script>
 
 <template>
--- src/views/Toto.vue
+++ src/views/Toto.vue
@@ -13,6 +13,7 @@ export default {
   name: 'Toto',
   components: {
     Bonne
-  }
+  },
 }
+
 </script>
